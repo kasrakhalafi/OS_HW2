5a51c02008173332a4c04bc6a342cdc2228de67d
kasra khalafi <kasrakhalafi@gmail.com>
2019-06-30 10:27:18 +0430

    frrTest, RRsanity and Gsaniy completed

--- Gsanity.c
+++ Gsanity.c
@@ -5,25 +5,23 @@
 
 int main(int argc, char *argv[]) {
   int pid;
+  int fatherPID = getpid();
 
-
-
-  if (argc != 2) {
-    printf(2, "usage: %s n\n", argv[0]);
-  }
-  printf("Father pid is <%s>\n",getpid() );
+  printf("Father pid is <%s>\n",fatherPID);
   sleep(10);
   pid = fork();
   if ( pid < 0 )
    {
-    printf(1, "%d failed in fork!\n", getpid());
+     for (size_t i = 0; i < 1000; i++){
+         printf(1,"process <%d> prints for the %d time",getpid(),i);
+     }
     exit();
-   } else if (pid == 0)
-   {
-    for (size_t i = 0; i < 1000; i++)
-    {
-        printf("process <%d> prints for the %d time",getpid(),i);
+   } else{
+    for (size_t i = 0; i < 1000; i++){
+        printf(1,"process <%d> prints for the %d time",getpid(),i);
     }
+    
   exit();
+  wait();
   }
 }
